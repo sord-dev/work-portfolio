c7290a56c624ace647528e9773439e12253cab69
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-09-11 13:01:07 +0100

    adding cypress and changing text selection color

--- src/api/server.js
+++ src/api/server.js
@@ -2,6 +2,8 @@ import * as dotenv from "dotenv";
 import axios from "axios";
 dotenv.config();
 
+// i may have to change the database and cms..
+
 export const strapiServer = axios.create({
   baseURL: "http://localhost:1337/api",
 });
