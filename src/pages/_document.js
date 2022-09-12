24fd0559eb9132559d2c24af1240d3249f4c083c
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-09-12 17:22:01 +0100

    changing lang tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en-GB">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'true'} />
